3a08795d9ad1c4ef1f550c0a625a37325e096410
seto <seto77@gmail.com>
2023-01-20 12:31:27 +0900

    update

--- PDIndexer/Version.cs
+++ PDIndexer/Version.cs
@@ -25,6 +25,7 @@ static class Version
     /// </summary>
     static public string History = 
         "History" +
+        "\r\n ver4.424(2023/01/20) Improved 'Fitting peaks'. Fixed design bugs on Japanese mode." +
         "\r\n ver4.423(2023/01/19) Improved macro functions. Improved 'Sequential Analysis'" +
         "\r\n ver4.421(2023/01/18) Fixed a minor bug on 'Fitting Diffraction Peaks'. " +
         "\r\n ver4.420(2023/01/07) Improved 'Sequential Analysis'. " +
